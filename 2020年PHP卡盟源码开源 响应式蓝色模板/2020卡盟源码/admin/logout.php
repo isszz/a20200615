5debaa47249b5d00c30a6da39b4300396ec88fd7
isszz <isszz@qq.com>
2022-07-23 23:30:08 +0800

    Delete 2020年PHP卡盟源码开源 响应式蓝色模板/2020卡盟源码 directory

--- 2020年PHP卡盟源码开源 响应式蓝色模板/2020卡盟源码/admin/logout.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-session_start();
-unset($_SESSION['ysk_username']);
-unset($_SESSION['ysk_flag']);
-unset($_SESSION['ysk_founder']);
-echo "<script language=\"javascript\">alert('你已成功退出！');window.location.href='login.php';</script>";
-?>
--- 2020年PHP卡盟源码开源 响应式蓝色模板/2020卡盟源码/xy.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include_once('jhs_config/function.php');
-?>
-<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
-<?=$login_reg?>
-    
